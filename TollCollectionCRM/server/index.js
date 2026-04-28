81b65010049d6e823300511119afbed329a4e715
seanjcarley <16079050+seanjcarley@users.noreply.github.com>
2026-04-28 18:28:53 +0000

    commented out console.log(app) on index.js also added dev container

--- TollCollectionCRM/server/index.js
+++ TollCollectionCRM/server/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors);
+
+// console.log(app);
 
 app.listen(8080, () => {
     console.log('Server listening on Port 8080!');
